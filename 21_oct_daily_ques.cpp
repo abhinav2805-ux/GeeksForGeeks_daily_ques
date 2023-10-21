c247282f4991136cf8348de374681762c7afb59f
Abhinav Gupta <128059655+abhinav2805-ux@users.noreply.github.com>
2023-10-21 05:52:17 +0530

    Create 21_oct_daily_ques.cpp

---
+++ 21_oct_daily_ques.cpp
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+                                                             //Sum of all divisors from 1 to n
+
+
+
+
+
+
+
+class Solution
+{
+public:
+    long long sumOfDivisors(int N)
+    {
+         long long sum = 0;
+        for(int i = 1; i <= N; i++){
+            if(N == 0)
+             return 0;
+             sum += (N / i) * i;
+        }
+        return sum;
+    }
+};
